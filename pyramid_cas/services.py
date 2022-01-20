7723c7eaa395533fde470edfd942c93be5bb77c8
Pascal Isman <gitcom@isman.fr>
2022-01-20 18:13:02 +0100

    fix import
    ImportError: cannot import name 'urlencode' from 'urllib'

--- pyramid_cas/services.py
+++ pyramid_cas/services.py
@@ -1,6 +1,6 @@
 import logging
-from urllib import urlencode, urlopen
-from urlparse import urljoin
+from urllib.parse import urlencode, urljoin
+from urllib.request import urlopen
 from xml.etree import ElementTree
 
 from pyramid.httpexceptions import HTTPFound
